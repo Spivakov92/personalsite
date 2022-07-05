2a3dbe0da558600c4206b6976e7df81324d414ef
Spivakov92 <sagesk8@gmail.com>
2022-07-05 19:05:12 +0300

    about me part

--- portfolio site/my-app/src/components/about/About.jsx
+++ portfolio site/my-app/src/components/about/About.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './about.scss'
+import React from "react";
+import "./about.scss";
+import Me from "../../assets/myself2.png";
+import { FaUniversity, FaAward } from "react-icons/fa";
 
 const about = () => {
   return (
-    <section id='about' >About</section >
-  )
-}
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="myself" />
+          </div>
+        </div>
 
-export default about
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaUniversity className="about__card-icon" />
+              <h5>Educatiuon</h5>
+              <small>architecrure - bachelore</small>
+            </article>
+            <article className="about__card">
+              <FaAward className="about__card-icon" />
+              <h5>Certification</h5>
+              <small>Front end development - Logos IT academy</small>
+            </article>
+            {/* <article className="about__card">
+              <FaUniversity className="about__card-icon" />
+              <h5>Educatiuon</h5>
+              <small>architecrure - bachelore</small>
+            </article> */}
+          </div>
+
+          <p>
+            Passionate about creating web applications and working with
+            cutting-edge technologies. Always ready to learn new technologies
+            and improve skills. Fast learner. Open to challenges, responsible
+            and punctual.
+          </p>
+          <a href="#contacts" className="btn btn-primary">Hire me</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default about;
--- portfolio site/my-app/src/components/nav/Nav.jsx
+++ portfolio site/my-app/src/components/nav/Nav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./nav.scss";
 import {
   AiOutlineHome,
@@ -8,18 +9,35 @@ import {
 } from "react-icons/ai";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <AiOutlineBook />
       </a>
-      <a href="#contacts">
+      <a
+        href="#contacts"
+        onClick={() => setActiveNav("#contacts")}
+        className={activeNav === "#contacts" ? "active" : ""}
+      >
         <AiOutlinePhone />
       </a>
     </nav>
