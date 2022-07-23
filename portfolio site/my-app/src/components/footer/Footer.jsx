d1533b020bca40f46b66c20da0c4eda2e52d035e
Spivakov92 <sagesk8@gmail.com>
2022-07-23 17:41:10 +0300

    footer

--- portfolio site/my-app/src/components/footer/Footer.jsx
+++ portfolio site/my-app/src/components/footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './footer.scss'
+import {BsLinkedin , BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+       <div className='footer__socials'>
+        <a href="https://www.linkedin.com/in/arsen-spivakov-38b17323b/" target='_blank'><BsLinkedin/></a>
+        <a href="https://github.com/Spivakov92/personalsite" target='_blank'><BsGithub/></a>
+    </div>
+    </footer>
   )
 }
 
